e60376cd24f9818fa467c3ac96f604da4d3deb53
adiaz-racowireless <adiaz@racowireless.com>
2015-10-23 15:02:15 -0400

    Initial commit with preliminar version

---
+++ Play.Stopwatch/MainActivity.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Play.Stopwatch.Views;
+
+namespace Play.Stopwatch
+{
+    [Activity(Label = "Play.Stopwatch", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        TextView _digitalTimer;
+        StopwatchView _stopwatchView;
+
+        Button _startButton;
+        Button _stopButton;
+        Button _resetButton;
+
+        private readonly System.Diagnostics.Stopwatch _stopwatch = new System.Diagnostics.Stopwatch();
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            _digitalTimer = FindViewById<TextView>(Resource.Id.DigitalTimer);
+            _stopwatchView = FindViewById<StopwatchView>(Resource.Id.Stopwatch);
+
+            _startButton = FindViewById<Button>(Resource.Id.StartButton);
+            _stopButton = FindViewById<Button>(Resource.Id.StopButton);
+            _resetButton = FindViewById<Button>(Resource.Id.ResetButton);
+
+            _startButton.Click += Start;
+            _stopButton.Click += Stop;
+            _resetButton.Click += Reset;
+
+            _stopButton.Enabled = false;
+        }
+
+        private void Start(object o, EventArgs args)
+        {
+            _stopwatch.Start();
+            _stopButton.Enabled = true;
+            _startButton.Enabled = false;
+
+            Timer();
+        }
+
+        private void Stop(object o, EventArgs args)
+        {
+            _stopwatch.Stop();
+            _stopButton.Enabled = false;
+            _startButton.Enabled = true;
+        }
+
+        private void Reset(object o, EventArgs args)
+        {
+            _stopwatch.Reset();
+            _stopButton.Enabled = false;
+            _startButton.Enabled = true;
+
+            RefreshTimer();
+        }
+
+        async void Timer()
+        {
+            while (true)
+            {
+                await Task.Delay(50);
+
+                RefreshTimer();
+
+                if (!_stopwatch.IsRunning) break;
+            }
+        }
+
+        private void RefreshTimer()
+        {
+            var span = TimeSpan.FromMilliseconds(_stopwatch.ElapsedMilliseconds);
+
+            _stopwatchView.Refresh((int)Math.Floor(span.TotalMinutes), span.Seconds);
+
+            _digitalTimer.Text = $"{Math.Floor(span.TotalMinutes).ToString("00")}:" +
+                                 $"{span.Seconds.ToString("00")}." +
+                                 $"{span.Milliseconds.ToString("000")}";
+        }
+    }
+}
+
---
+++ Play.Stopwatch/Views/StopwatchView.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Android.Content;
+using Android.Graphics;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+
+namespace Play.Stopwatch.Views
+{
+    [Register("play.stopwatch.views.StopwatchView")] 
+    public sealed class StopwatchView : View
+    {
+        private int _seconds;
+        private int _minutes;
+
+        private Path _minuteHand;
+        private Path _secondHand;
+
+        private static readonly DashPathEffect MinuteDashEffect = new DashPathEffect(new[] {0.1f, 3*(float) Math.PI - 0.1f}, 10);
+        private static readonly DashPathEffect FifthMinuteDashEffect = new DashPathEffect(new[] {0.1f, 15*(float) Math.PI - 0.1f}, 0);
+
+        public StopwatchView(Context context) : base(context)
+        {
+            Initialize();
+        }
+
+        public StopwatchView(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+            Initialize();
+        }
+
+        public StopwatchView(Context context, IAttributeSet attrs, int defStyle) : base(context, attrs, defStyle)
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            _minuteHand = new Path();
+            _minuteHand.MoveTo(0, 10);
+            _minuteHand.LineTo(0, -55);
+            _minuteHand.Close();
+
+            _secondHand = new Path();
+            _secondHand.MoveTo(0, -80);
+            _secondHand.LineTo(0, 15);
+            _secondHand.Close();
+        }
+
+        public void Refresh(int minutes, int seconds)
+        {
+            if (seconds != _seconds)
+                Invalidate();
+
+            _seconds = seconds;
+            _minutes = minutes;
+        }
+
+        protected override void OnDraw(Canvas canvas)
+        {
+            base.OnDraw(canvas);
+            
+            var paint = new Paint
+            {
+                Color = new Color(45, 45, 45)
+            };
+
+            canvas.DrawPaint(paint);
+
+            DrawStopwatch(canvas);
+            DrawSecondsHand(canvas);
+            DrawMinutesHand(canvas);
+        }
+
+        public void DrawStopwatch(Canvas canvas)
+        {
+            canvas.Save();
+            canvas.Translate(Width / 2F, Height / 2F);
+            
+            var tickMarks = new Path();
+            tickMarks.AddCircle(0, 0, 90, Path.Direction.Cw);
+
+            var scale = Math.Min(Width, Height) / 2F / 120;
+            canvas.Scale(scale, scale);
+
+            var paint = new Paint
+            {
+                StrokeCap = Paint.Cap.Square,
+                Color = new Color(240, 240, 240)
+            };
+
+            paint.SetStyle(Paint.Style.Stroke);
+            paint.StrokeWidth = 3;
+            paint.SetPathEffect(MinuteDashEffect);
+            canvas.DrawPath(tickMarks, paint);
+
+            paint.Color = new Color(240, 240, 240);
+            paint.StrokeWidth = 4;
+            paint.SetPathEffect(FifthMinuteDashEffect);
+            canvas.DrawPath(tickMarks, paint);
+        }
+
+        public void DrawSecondsHand(Canvas canvas)
+        {
+            canvas.Save();
+            canvas.Rotate(_seconds * 6);
+            
+            var paint = new Paint
+            {
+                Color = Color.Red,
+                StrokeWidth = 2
+            };
+            paint.SetStyle(Paint.Style.Stroke);
+
+            canvas.DrawPath(_secondHand, paint);
+            canvas.Restore();
+        }
+
+        public void DrawMinutesHand(Canvas canvas)
+        {
+            canvas.Save();
+            canvas.Rotate(_minutes * 6);
+            
+            var paint = new Paint
+            {
+                Color = new Color(206, 206, 206),
+                StrokeWidth = 4
+            };
+            paint.SetStyle(Paint.Style.Stroke);
+
+            canvas.DrawPath(_minuteHand, paint);
+            canvas.Restore();
+        }
+    }
+}
